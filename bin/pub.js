3003eaf3bfd31b7f711a6538c5443136e6ba8d23
Asfandyar Sheikh <andy@replyqa.com>
2023-04-07 06:42:55 +0500

    woohoo

--- bin/pub.js
+++ bin/pub.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function pub() {
     const {stdout, stderr} = await exec(
-        `npm version patch && npm publish --access=public`,
+        `git add -A && git commit -am "woohoo" && npm version patch && npm publish --access=public && git push --all`,
     );
     console.log(stdout);
     console.log(stderr);
