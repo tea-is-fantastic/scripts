f3a893d5711c5a529475f322c4426d842496d6c1
Asfandyar Sheikh <andy@replyqa.com>
2023-04-25 05:56:36 +0500

    woohoo

--- bin/build.js
+++ bin/build.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function build() {
     const {stdout, stderr} = await exec(
-        `npm i && npm run build && git add -A && git commit --no-verify -am "woohoo" && git push --all && rm -rf ./node_modules`,
+        `npm i && npm run build && git add -A && git commit --no-verify -am "woohoo" && git push --all && rm -rf ./node_modules && rm -rf ./build`,
     );
     console.log(stdout);
     console.log(stderr);
